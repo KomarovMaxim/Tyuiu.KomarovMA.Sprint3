1daeb593a230d874b4b6b24267cd14ebfc281dd0
Tyuiu.KomarovMA@outlook.com <Tyuiu.KomarovMA@outlook.com>
2023-10-28 22:39:27 +0500

    Task4

---
+++ Tyuiu.KomarovMA.Sprint3.Task4.V14.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KomarovMA.Sprint3.Task4.V14.Lib;
+namespace Tyuiu.KomarovMA.Sprint3.Task4.V14.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CalculateTest()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(6.263, ds.Calculate(-5, 5));
+        }
+    }
+}
---
+++ Tyuiu.KomarovMA.Sprint3.Task4.V14/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KomarovMA.Sprint3.Task4.V14.Lib;
+namespace Tyuiu.KomarovMA.Sprint3.Task4.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Комаров М.А. | СМАРТБ 23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Получение результата из switch                                    *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Комаров Максим Александрович | СМАРТБ 23-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение    * ");
+            Console.WriteLine("* функции y=cos(x)/sin(x) При х = 0 прервать цикл. Полученные значения .  *");
+            Console.WriteLine("* суммировать                                                             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.Write("Введите начало отрезка: ");
+
+
+            int start = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите конец отрезка: ");
+            int end = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(start, end));
+            Console.ReadKey();
+        }
+    }
+}
